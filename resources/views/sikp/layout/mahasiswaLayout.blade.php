5c1c0c3562c5c050c2961795176de145aa1be8a8
LAPTOP-6E6G0R3F\ASUS <ristri.krisnugraheni@si.ukdw.ac.id>
2021-05-19 10:30:10 +0700

    commit kedua

--- resources/views/sikp/layout/mahasiswaLayout.blade.php
+++ resources/views/sikp/layout/mahasiswaLayout.blade.php
@@ -205,12 +205,6 @@
 				<button type="button" class="close" data-dismiss="alert">×</button>
 				<strong>{{ $message }}</strong>
 			</div>
-			@endif
-
-			@if ($message = Session::get('info'))
-			<div class="alert alert-info alert-block">
-				<button type="button" class="close" data-dismiss="alert">×</button>
-				<strong>{{ $message }}</strong>
 			</div>
 			@endif
 
